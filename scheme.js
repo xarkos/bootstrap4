3dda4a29d3d2bf1ee4acf2b90ec33f0a78aab74d
Gordon E. Leek <gordon_leek@yahoo.com>
2017-05-03 07:54:43 +0100

    Initial commit

---
+++ scheme.js
@@ -0,0 +1,18 @@
+<script type="application/ld+json">
+{ "@context": "http://schema.org", 
+ "@type": "Article",
+ "headline": "The Alchemical Algorithm",
+ "alternativeHeadline": "How to transmute any area of your life Love, Health, Wealth, Romance, Happiness & Success literally from lead to gold.",
+ "image": "http://alchemicalalgorithm.000webhostapp.com/logo.png",
+ "author": "Pete Munrab", 
+ "genre": "Esoteric Knowledge", 
+ "keywords": "alchemy science mysticism, self development get rich", 
+ "publisher": "Pete Munrab",
+ "url": "http://alchemicalalgorithm.000webhostapp.com/",
+ "datePublished": "2017-05-01",
+ "dateCreated": "2017-05-01",
+ "dateModified": "2017-05-01",
+ "description": "How to transmute any area of your life Love, Health, Wealth, Romance, Happiness & Success literally from lead to gold by using The Ultimate Secret of the Ages.",
+ "articleBody": "I have always had an enquiring mind and I've always been ambitious too. Having being born into a working class family and raised on a housing estate north of the Great City of Manchester I had a burning desire for physical, financial and mental improvement and a drive to get out into the great big world and grab what was mine. Unlike most of the factory fodder at my school I  later graduated from a red brick university with a broad based degree in the Sciences and, at the time, thought the world would be at my feet. However my education was only just starting. "
+ }
+</script>
